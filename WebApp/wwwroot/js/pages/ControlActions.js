fe09abe2be08e35c4d26bbc01e1bc4120e2d0287
Daniel <dcamposs@ucenfotec.ac.cr>
2025-07-09 00:54:16 +0000

    change over URL API to use deployed API

--- WebApp/wwwroot/js/pages/ControlActions.js
+++ WebApp/wwwroot/js/pages/ControlActions.js
@@ -1,6 +1,6 @@
 ﻿function ControlActions() {
 	//Ruta base del API
-	this.URL_API = "https://localhost:7103/api/";
+	this.URL_API = "https://cenfocinemas-dcampos-dgdyezh5b0cyb6be.eastus2-01.azurewebsites.net/api/";
 
 	this.GetUrlApiService = function (service) {
 		return this.URL_API + service;
